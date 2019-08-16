50f91dc28439e31077e1ee6fe4e578fbde67350d
root <BoHongtao@yeah.net>
2019-08-16 18:25:55 +0800

    day2

---
+++ error_catch.lua
@@ -0,0 +1,7 @@
+
+function add(a)
+	assert(type(a) == 'number',"参数不是一个数字")
+    return a
+end
+
+add('icuhij')
---
+++ module.lua
@@ -0,0 +1,5 @@
+module = {}
+module.constant = "我在包中定义的常量"
+function module.func1()
+	io.write("这是全局一个函数")
+end
